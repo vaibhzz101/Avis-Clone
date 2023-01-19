8b95c3a642ab59dca99632052051ed3dfdc74c3b
rajsandeep257 <rajsandeep257@gmail.com>
2023-01-19 22:32:16 +0530

    day-3 commiy

---
+++ avis/checkout.js
@@ -0,0 +1,40 @@
+let cararr = [
+    {name:"Swift Dezire",
+     price:7890,
+     transmission:"MAnual",
+     doors:4,
+     image:"https://www.avis.co.in/upload/model/1010dzire.png",
+     seat:4,
+     luggage:2,
+   
+    },
+    // {name:"City Of Dreams",
+    //  price:9487,
+    //  transmission:"MAnual",
+    //  doors:4,
+    //  image:"https://www.avis.co.in/upload/model/10121012Honda-City.png",
+    //  seat:4,
+    //  luggage:2,
+   
+    // },
+    // {name:"Camry Accord of similar",
+    //  price:7890,
+    //  transmission:"MAnual",
+    //  doors:4,
+    //  image:"https://www.avis.co.in/upload/model/1014camry1.png",
+    //  seat:4,
+    //  luggage:2,
+   
+    // },
+    // {name:"BMW 5",
+    //  price:78900,
+    //  transmission:"Automatic",
+    //  doors:4,
+    //  image:"https://www.avis.co.in/upload/model/10151022BMW5series.png",
+    //  seat:4,
+    //  luggage:2,
+   
+    // }
+]
+
+localStorage.setItem("car-array",JSON.stringify(cararr))
